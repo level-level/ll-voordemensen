e30702e8abaf11ffa464e62c9e52c087476ea2fd
menno-ll <menno@level-level.com>
2020-10-21 17:47:23 +0200

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+[![GitHub Actions status](https://github.com/level-level/ll-voordemensen/workflows/Build%20%26%20test/badge.svg)](https://github.com/level-level/ll-voordemensen/actions)
+
+
 # voordemensen
 Unofficial plugin to access the VoordeMensen ticket platform directly from WordPress
---
+++ ll-voordemensen.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin Name:  VoordeMensen
+ * Version:      0.0.1
+ * Description:  Unofficial plugin to access the VoordeMensen ticket platform directly from WordPress
+ * Author:       Level Level
+ * Author URI:   https://www.level-level.com
+ *
+ * Text Domain: ll-vdm
+ * Domain Path: /languages/
+ */
+
+add_action(
+	'plugins_loaded',
+	function() {
+		require_once ABSPATH . 'wp-admin/includes/plugin.php';
+
+		$plugin_path = realpath( plugin_dir_path( __FILE__ ) ) . DIRECTORY_SEPARATOR;
+		$plugin_data = get_plugin_data( __FILE__ );
+		define( 'LL_VDM_PLUGIN_VERSION', isset( $plugin_data['Version'] ) ? $plugin_data['Version'] : '' );
+
+		$autoload_file = $plugin_path . 'vendor/autoload.php';
+
+		if ( file_exists( $autoload_file ) ) {
+			include_once $autoload_file;
+		}
+
+		// Register hooks
+
+		// Load textdomain
+		load_plugin_textdomain( 'll-vdm', false, basename( dirname( __FILE__ ) ) . '/languages/' );
+	}
+);
