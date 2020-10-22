4d01ffd32f55baee2fe91757d1f06a295ad83662
menno-ll <menno@level-level.com>
2020-10-22 10:27:39 +0200

    add filters to apihelper

--- app/Utilities/APIHelper.php
+++ app/Utilities/APIHelper.php
@@ -7,6 +7,7 @@ use DateTimeZone;
 use LevelLevel\VoorDeMensen\Objects\Event;
 use LevelLevel\VoorDeMensen\Objects\SubEvent;
 use LevelLevel\VoorDeMensen\Utilities\Image as ImageUtil;
+use WP_Error;
 
 class APIHelper {
 
@@ -24,6 +25,8 @@ class APIHelper {
 			$event_id = $event->get_id();
 		}
 
+		do_action( 'll_vdm_before_insert_event', $event_id, $api_event );
+
 		// Update post object
 		$post_data = array(
 			'ID'           => $event_id,
@@ -33,22 +36,25 @@ class APIHelper {
 			'post_name'    => sanitize_title( $api_event->event_name ),
 			'post_content' => '<p>' . esc_html( $api_event->event_text ) . '</p>',
 		);
-		$post_id   = wp_insert_post( $post_data );
+		$post_data = apply_filters( 'll_vdm_update_event_post_data', $post_data, $api_event );
 
-		if ( ! $post_id || is_wp_error( $post_id ) ) {
+		$event_id   = wp_insert_post( $post_data );
+		if ( ! $event_id || $event_id instanceof WP_Error ) {
 			return 0;
 		}
 
 		// Set thumbnail
 		if ( ! empty( $api_event->event_image ) ) {
 			$image_util = new ImageUtil();
-			$image_util->set_post_thumbnail( $post_id, $api_event->event_image );
+			$image_util->set_post_thumbnail( $event_id, $api_event->event_image );
 		}
 
 		// Update meta
-		update_post_meta( $post_id, 'vdm_id', $api_event->event_id );
+		update_post_meta( $event_id, 'vdm_id', $api_event->event_id );
 
-		update_post_meta( $post_id, 'short_text', $api_event->event_short_text ?? null );
+		update_post_meta( $event_id, 'short_text', $api_event->event_short_text ?? null );
+
+		do_action( 'll_vdm_after_insert_event', $event_id, $api_event );
 
 		if ( isset( $api_event->sub_events ) && is_array( $api_event->sub_events ) ) {
 			foreach ( $api_event->sub_events as $api_sub_event ) {
@@ -72,6 +78,8 @@ class APIHelper {
 			$sub_event_id = $sub_event->get_id();
 		}
 
+		do_action( 'll_vdm_before_insert_sub_event', $event_id, $api_sub_event );
+
 		$status = 'draft';
 		if ( isset( $api_sub_event->event_status ) && $api_sub_event->event_status === 'pub' ) {
 			$status = 'publish';
@@ -86,26 +94,27 @@ class APIHelper {
 			'post_name'    => sanitize_title( $api_sub_event->event_name ),
 			'post_content' => '<p>' . esc_html( $api_sub_event->event_text ) . '</p>',
 		);
-		$post_id   = wp_insert_post( $post_data );
+		$post_data = apply_filters( 'll_vdm_update_sub_event_post_data', $post_data, $api_sub_event, $event_id );
 
-		if ( ! $post_id || is_wp_error( $post_id ) ) {
+		$event_id   = wp_insert_post( $post_data );
+		if ( ! $event_id || $event_id instanceof WP_Error ) {
 			return 0;
 		}
 
 		// Set thumbnail
 		if ( ! empty( $api_sub_event->event_image ) ) {
 			$image_util = new ImageUtil();
-			$image_util->set_post_thumbnail( $post_id, $api_sub_event->event_image );
+			$image_util->set_post_thumbnail( $event_id, $api_sub_event->event_image );
 		}
 
 		// Update meta
-		update_post_meta( $post_id, 'vdm_id', $api_sub_event->event_id );
-		update_post_meta( $post_id, 'event_id', $event_id );
-		update_post_meta( $post_id, 'vdm_event_id', $api_sub_event->event_main_id );
-		update_post_meta( $post_id, 'vdm_location_id', $api_sub_event->location_id ?? null );
+		update_post_meta( $event_id, 'vdm_id', $api_sub_event->event_id );
+		update_post_meta( $event_id, 'event_id', $event_id );
+		update_post_meta( $event_id, 'vdm_event_id', $api_sub_event->event_main_id );
+		update_post_meta( $event_id, 'vdm_location_id', $api_sub_event->location_id ?? null );
 
-		update_post_meta( $post_id, 'short_text', $api_sub_event->event_short_text ?? null );
-		update_post_meta( $post_id, 'url', $api_sub_event->event_url ?? null );
+		update_post_meta( $event_id, 'short_text', $api_sub_event->event_short_text ?? null );
+		update_post_meta( $event_id, 'url', $api_sub_event->event_url ?? null );
 
 		$start_timestamp = null;
 		$end_timestamp = null;
@@ -132,10 +141,12 @@ class APIHelper {
 			}
 		}
 
-		update_post_meta( $post_id, 'start_date', $start_timestamp );
-		update_post_meta( $post_id, 'end_date', $start_timestamp );
-		update_post_meta( $post_id, 'rep', $api_sub_event->event_rep ?? null );
-		update_post_meta( $post_id, 'max_tickets_per_order', $api_sub_event->event_free ?? null );
-		update_post_meta( $post_id, 'location_name', $api_sub_event->location_name ?? null );
+		update_post_meta( $event_id, 'start_date', $start_timestamp );
+		update_post_meta( $event_id, 'end_date', $start_timestamp );
+		update_post_meta( $event_id, 'rep', $api_sub_event->event_rep ?? null );
+		update_post_meta( $event_id, 'max_tickets_per_order', $api_sub_event->event_free ?? null );
+		update_post_meta( $event_id, 'location_name', $api_sub_event->location_name ?? null );
+
+		do_action( 'll_vdm_after_insert_sub_event', $event_id, $api_sub_event );
 	}
 }
