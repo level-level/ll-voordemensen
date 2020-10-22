975152637d546ef6615ef3d563aa4b8d517b4e70
menno-ll <menno@level-level.com>
2020-10-22 12:43:06 +0200

    fix metabox template errors

--- app/MetaBox.php
+++ app/MetaBox.php
@@ -12,6 +12,11 @@ class MetaBox {
 	}
 
 	public function add_meta_box( string $post_type ) {
+		$client_name = get_option( 'll_vdm_api_client_name', null );
+		if ( empty( $client_name ) ) {
+			return;
+		}
+
 		add_meta_box(
 			'll_vdm_id',
 			__( 'VoordeMensen event', 'll-vdm' ),
--- templates/admin/metabox.php
+++ templates/admin/metabox.php
@@ -3,17 +3,22 @@
 /**
  * Template for the admin metabox dropdown field
  *
- * @var \LevelLevel\VoorDeMensen\Objects\Event $event
+ * @var \LevelLevel\VoorDeMensen\Objects\Event|null $event
  * @var array $api_events
  */
+
+$event_vdm_id = 0;
+if ( $event instanceof LevelLevel\VoorDeMensen\Objects\Event ) {
+	$event_vdm_id = $event->get_vdm_id();
+}
 
 ?>
 
 <label for="vdm_event_id"><?php esc_html_e( 'VoordeMensen event', 'll-vdm' ) ?></label>
-<select name="vdm_event_id" id="vdm_event_id" class="postbox" >
+<select name="vdm_event_id" id="vdm_event_id" >
 	<?php foreach ($api_events as $api_event) { ?>
 		<option value=""><?php echo esc_html_e( 'Select a VoordeMensen event', 'll-vdm' ); ?></option>
-		<option value="<?php echo esc_attr( $api_event->event_id ) ?>" <?php selected( (int) $api_event->event_id, $event->get_vdm_id() ); ?>>
+		<option value="<?php echo esc_attr( $api_event->event_id ) ?>" <?php selected( (int) $api_event->event_id, $event_vdm_id, true ); ?>>
 			<?php echo esc_html( $api_event->event_id . ' | ' . $api_event->event_name ); ?>
 		</option>
 	<?php } ?>
