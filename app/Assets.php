b58def2a9659ee31bf7a1d7a4b96228b4f5063eb
menno-ll <menno@level-level.com>
2020-12-02 14:54:27 +0100

    change url

--- app/Assets.php
+++ app/Assets.php
@@ -93,9 +93,9 @@ class Assets {
 		}
 
 		$display_type = ( new TicketSalesScreenTypeSetting() )->get_value();
-		$src          = 'https://tickets.voordemensen.nl/' . rawurlencode( $client_name ) . '/iframes/vdm_loader.js';
+		$src          = 'https://tickets.voordemensen.nl/' . rawurlencode( $client_name ) . '/event/vdm_loader.js';
 		if ( $display_type === 'side' ) {
-			$src = 'https://tickets.voordemensen.nl/' . rawurlencode( $client_name ) . '/iframes/vdm_sideloader.js';
+			$src = 'https://tickets.voordemensen.nl/' . rawurlencode( $client_name ) . '/event/vdm_sideloader.js';
 		}
 
 		wp_enqueue_script( 'll_vdm_external_script', $src, array(), LL_VDM_PLUGIN_VERSION, true );
