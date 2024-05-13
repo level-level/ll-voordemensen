296f0132b9ff661d14b6bf6e1bd19a2c3e8f4036
Menno van den Ende <50165380+menno-ll@users.noreply.github.com>
2024-05-13 13:58:51 +0200

    Update ll-voordemensen.php

--- ll-voordemensen.php
+++ ll-voordemensen.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:  VoordeMensen
- * Version:      1.5.0
+ * Version:      1.6.0
  * Description:  Unofficial plugin to access the VoordeMensen ticket platform directly from WordPress
  * Author:       Level Level
  * Author URI:   https://www.level-level.com
